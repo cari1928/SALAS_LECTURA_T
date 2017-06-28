301794174c0ea8475ec66af2fd2230e0d5a519fa
Cari Santana <radogan1928@hotmail.com>
2017-06-27 22:49:39 -0500

    Solucion Formato Preguntas

--- admin/formato_preguntas.php
+++ admin/formato_preguntas.php
@@ -12,28 +12,28 @@ if ($cveperiodo == "") {
 }
 
 if (isset($_GET['accion'])) {
-  
-  switch ($_GET['accion']) {
-      
-      case 'fileinput':
-        $dir_subida ="/home/ubuntu/workspace/pdf" . "/" . $cveperiodo . "/";
-      
-        if($_FILES['datos']['size']['archivo'] > 1000000){
-          message('danger', 'El archivo es mayor a un MB.', $web);
-        }
-        if($_FILES['datos']['type']['archivo'] != 'application/pdf'){
-             message('danger', 'Solo esta permitido subir archivos de tipo .pdf', $web);
-       }
 
-       $nombre = "formato_preguntas.pdf";
-       if (move_uploaded_file($_FILES['datos']['tmp_name']['archivo'], $dir_subida.$nombre)){
-         header('Location: index.php?aviso=1');
-       }else{
-         header('Location: index.php?aviso=2');
-       }
+  switch ($_GET['accion']) {
+
+    case 'fileinput':
+      $dir_subida = "/home/slslctr/pdf" . "/" . $cveperiodo . "/";
+
+      if ($_FILES['datos']['size']['archivo'] > 1000000) {
+        message('danger', 'El archivo es mayor a un MB.', $web);
+      }
+      if ($_FILES['datos']['type']['archivo'] != 'application/pdf') {
+        message('danger', 'Solo esta permitido subir archivos de tipo .pdf', $web);
+      }
+
+      $nombre = "formato_preguntas.pdf";
+      if (move_uploaded_file($_FILES['datos']['tmp_name']['archivo'], $dir_subida . $nombre)) {
+        header('Location: index.php?aviso=1');
+      } else {
+        header('Location: index.php?aviso=2');
+      }
       break;
   }
-  
+
 }
 
 $web->smarty->display('formato_preguntas.html');
